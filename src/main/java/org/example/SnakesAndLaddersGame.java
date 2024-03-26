f722c15a7a379f8754c0f564a5ad70d98fba38ce
Divanshu Joshi <divajosh@amazon.com>
2024-03-26 21:57:28 +0530

    Further Restructure

--- src/main/java/org/example/SnakesAndLaddersGame.java
+++ src/main/java/org/example/SnakesAndLaddersGame.java
@@ -7,6 +7,11 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class SnakesAndLaddersGame {
+    private final static String MANUAL = "manual";
+    private final static String AUTO = "auto";
+    private final static String NORMAL = "normal";
+    private final static String FILE_PATH = "/Users/divajosh/Downloads/SnakesAndLaddersGameSolution/src/main/java/org/example/";
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         /*
@@ -15,18 +20,48 @@ public class SnakesAndLaddersGame {
         * and snakes and ladder positions manually
         * auto - automatic logic to generate snakes and ladders taking input in the json file and running the game.
         * */
-        String executionType = scanner.next();
-        if(executionType.equalsIgnoreCase("normal")) {
-            GameBoard gameBoard = new GameBoard(10, 1);
-            List<Player> players = new ArrayList<>();
-            DriverCode.manualInput(scanner, gameBoard, players);
-            GameExecution.normalGameExecution(players, gameBoard);
-            return;
+        String executionType = scanner.next().toLowerCase();
+        switch (executionType) {
+            case "normal":
+                normalGameExecution(scanner);
+                break;
+            case "auto":
+                automaticGameExecution(scanner);
+                break;
+            case "manual":
+                manualExecution(scanner);
+                break;
+            default:
+                System.out.println("Unexpected Input Provided Please Note \n" + "normal , auto and manual Expected");
+
         }
 
+        scanner.close();
+    }
 
-        // Read game configuration from JSON file
-        GameConfig gameConfig = GameConfigReader.readGameConfig("/Users/divajosh/Downloads/SnakesAndLaddersGameSolution/src/main/java/org/example/game_config.json");
+    public static void normalGameExecution(Scanner scanner) {
+        GameBoard gameBoard = new GameBoard(10, 1);
+        List<Player> players =new ArrayList<>();
+        DriverCode.manualInput(scanner, gameBoard, players);
+        GameExecution.normalGameExecution(players, gameBoard);
+        return;
+    }
+
+    public static void automaticGameExecution(Scanner scanner) {
+        GameConfig gameConfig = readGameConfigFromFile("game_config.json");
+        performGameExecution(gameConfig, AUTO, scanner);
+    }
+
+    public static void manualExecution(Scanner scanner) {
+        GameConfig gameConfig = readGameConfigFromFile("game_config.json");
+        performGameExecution(gameConfig, MANUAL, scanner);
+    }
+
+    private static GameConfig readGameConfigFromFile(String filename) {
+        return GameConfigReader.readGameConfig(FILE_PATH + filename);
+    }
+
+    private static void performGameExecution(GameConfig gameConfig, String executionType, Scanner scanner) {
         System.out.println(gameConfig);
 
         // Create game board with snakes, ladders, and special objects
@@ -39,16 +74,10 @@ public class SnakesAndLaddersGame {
             movementStrategy = gameConfig.getMovementStrategy();
         }
 
-
-        //Manual Configurations can be added here.
-        if (executionType.equalsIgnoreCase("manual")) {
+        if (executionType.equals(MANUAL)) {
             ManualOverrideHandler.performManualOverride(players, gameBoard, scanner);
         }
 
-        // Simulate game turns in a round-robin fashion
-        // Log game events
         GameExecution.simulateGame(players, gameBoard, movementStrategy, executionType, scanner);
-
-        scanner.close();
     }
 }
